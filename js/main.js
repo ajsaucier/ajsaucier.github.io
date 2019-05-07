b6674dc599fa2cd788daab56a9ec1173780e54bd
Adam Saucier <adamscr13@gmail.com>
2019-05-07 14:00:09 -0400

    Adding JavaScript cookie
    Adding a JS cookie to allow for dark theme to be carried throughout the site for as long as the user wants, unless it's turned off

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ themeSwitch.addEventListener('click', (e) => {
   body.classList.toggle('dark-theme');
   themeSwitch.setAttribute('aria-checked', themeSwitch.getAttribute('aria-checked') === 'true' ? 'false' : 'true');
   themeSwitchText.innerHTML = (themeSwitch.getAttribute('aria-checked') === 'true' ? 'Turn dark theme off' : 'Turn dark theme on');
+  document.cookie = (body.classList.contains('dark-theme') === true ? 'darktheme=true; path=/' : 'darktheme=; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/');;
 });
+
+(function () {
+  if (document.cookie) {
+    body.classList.add('dark-theme');
+    themeSwitch.setAttribute('aria-checked', true);
+    themeSwitchText.innerHTML = 'Turn dark theme off';
+  } else {
+    themeSwitch.setAttribute('aria-checked', false);
+    themeSwitchText.innerHTML = 'Turn dark theme on';
+  }
+})();
