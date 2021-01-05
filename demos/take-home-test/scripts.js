3be8a03a354f77e667aeef628ba0794427449285
Adam Saucier <adamscr13@gmail.com>
2021-01-05 11:27:41 -0500

    Created new folder for testing demos

---
+++ demos/take-home-test/scripts.js
@@ -0,0 +1,57 @@
+// Assign elements to variables
+const form = document.getElementsByTagName("form")[0];
+const username = document.getElementById("username");
+const usernameError = document.getElementById("username-error");
+const password = document.getElementById("password");
+const passwordError = document.getElementById("password-error");
+const loginError = document.getElementById("login-error");
+
+form.addEventListener("submit", function (event) {
+  // Clear login error text
+  loginError.textContent = "";
+
+  checkUsername(event);
+  checkPassword(event);
+  checkLoginMatch(event)
+
+  if (username.value === "") {
+    username.focus();
+  } else if (password.value === "") {
+    password.focus();
+  }
+});
+
+function checkLoginMatch(event) {
+// If fields are not empty but don't have the correct login, show the error
+  if (username.value.length > 0 && password.value.length > 0) {
+    if (username.value !== "level" || password.value !== "Access123") {
+      event.preventDefault();
+      setTimeout(function () {
+        loginError.textContent =
+          "The username and password combination was not recognized. Please try again.";
+      }, 1000);
+    }
+  }
+}
+
+function checkUsername(event) {
+  if (username.value === "") {
+    usernameError.removeAttribute("hidden");
+    username.setAttribute("aria-invalid", "true");
+    event.preventDefault();
+  } else {
+    usernameError.setAttribute("hidden", "true");
+    username.setAttribute("aria-invalid", "false");
+  }
+}
+
+function checkPassword(event) {
+  if (password.value === "") {
+    passwordError.removeAttribute("hidden");
+    password.setAttribute("aria-invalid", "true");
+    event.preventDefault();
+  } else {
+    passwordError.setAttribute("hidden", "true");
+    password.setAttribute("aria-invalid", "false");
+  }
+}
